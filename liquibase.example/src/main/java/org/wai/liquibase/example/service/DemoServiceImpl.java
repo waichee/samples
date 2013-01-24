26635aa5a35dac1893bbe3b635e8eb0758646751
wai <waicheeyau@gmail.com>
2013-01-24 15:10:02 +1100

    add minimal logging

--- liquibase.example/src/main/java/org/wai/liquibase/example/service/DemoServiceImpl.java
+++ liquibase.example/src/main/java/org/wai/liquibase/example/service/DemoServiceImpl.java
@@ -1,5 +1,7 @@
 package org.wai.liquibase.example.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,22 +15,24 @@ import java.util.List;
 @Service
 public class DemoServiceImpl implements DemoService {
 
+    Logger logger= LoggerFactory.getLogger(DemoServiceImpl.class);
+
   @Autowired
   private CarDao carDao;
 
   @Autowired
   private BikeDao bikeDao;
 
-
-
   @Transactional(readOnly = true)
   public List<Car> getAllCars() {
+      logger.info("Finding all cars");
     return carDao.findAll();
   }
 
   @Transactional(readOnly = true)
   public List<Bike> getAllBikes() {
-    return bikeDao.findAll();
+      logger.info("Finding all bikes");
+      return bikeDao.findAll();
   }
 
 
