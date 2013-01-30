993615c6752b03b867e1c1aa3d08976f0b0002e2
wai <waicheeyau@gmail.com>
2013-01-30 13:19:38 +1100

    move the java migration script

--- flyway.sample/src/main/java/db/migration/V1_2__Add_table_clob.java
+++ flyway.sample/src/main/java/db/migration/V1_2__Add_table_clob.java
@@ -1,17 +1,18 @@
-package org.wai.flyway.sample.db.migration;
+package db.migration;
 
 
+import com.googlecode.flyway.core.api.migration.spring.SpringJdbcMigration;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
  * Java-based migration.
  */
-public class V1_2__Add_table_clob {
+public class V1_2__Add_table_clob implements SpringJdbcMigration {
 
     public void migrate(JdbcTemplate jdbcTemplate) throws Exception {
         String ddl = "CREATE TABLE java_migrated_table (  "+
                         "code        char(5) CONSTRAINT firstkey PRIMARY KEY, "+
-                        "title       text(40) NOT NULL," +
+                        "title       text NOT NULL," +
                         "date_prod   date); ";
 
         jdbcTemplate.execute(ddl);
